2863a07d127f41cf5e0e9fc8a0ec1136e195411a
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-12-11 12:38:23 -0500

    Save to folder

--- screen.py
+++ screen.py
@@ -1,6 +1,8 @@
+# https://faun.pub/how-to-install-selenium-in-linux-e8928b2b709
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import datetime
 
 # Set path Selenium
 CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
@@ -14,6 +16,8 @@ chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(service=s, options=chrome_options)
 
+dt = datetime.datetime.now()
+
 driver.get("https://google.com")
-driver.save_screenshot("google.png")
+driver.save_screenshot("screenshots/{}_google.png".format(dt.strftime()))
 driver.close()
