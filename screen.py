502cc7bf1112d4d8b09cd8b526f1bde45bcff4c5
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-12-11 12:33:46 -0500

    Learning...

--- screen.py
+++ screen.py
@@ -1,7 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
+# Set path Selenium
+CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
+s = Service(CHROMEDRIVER_PATH)
+WINDOW_SIZE = "1920,1080"
 
-driver = webdriver.Firefox()
+# Options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(service=s, options=chrome_options)
+
 driver.get("https://google.com")
 driver.save_screenshot("google.png")
 driver.close()
